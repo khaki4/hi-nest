1379ab97eb4b8bb962b6688869386d235a0a3e01
jiwonjung <4dakto@gmail.com>
2021-04-18 23:50:49 +0900

    add test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -29,7 +29,7 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -46,14 +46,33 @@ describe('AppController (e2e)', () => {
   });
 
   describe('/movies/:id', () => {
-    it('GET', () => {
+    it('GET 200', () => {
       return request(app.getHttpServer()).get('/movies/1').expect(200);
     });
 
     it('GET 404', () => {
       return request(app.getHttpServer()).get('/movies/999').expect(404);
     });
-    it.todo('POST');
-    it.todo('PATCH');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
+
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2021,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
   });
 });
