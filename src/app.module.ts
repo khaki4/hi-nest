baefc57aca6dd114abd77ddae2e2f187fc0ddfad
jiwonjung <4dakto@gmail.com>
2021-04-18 15:32:22 +0900

    refactor module hierachy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
 import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [MoviesModule],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
